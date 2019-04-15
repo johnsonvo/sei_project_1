bff423b576dcffe4640dfc2e531fa83dac374d29
johnsonvo <JOHNSONMVO@GMAIL.COM>
2019-04-15 16:53:54 -0700

    Added image attribute to UserSchema

--- models/user.js
+++ models/user.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    
+
     fullName:{type:String, required:true},
     email:{type:String, required:true, lowercase:true,trim:true},
+    image: String,
     dob: String,
     products:String,
     created_date:{type:Date, default:Date.now},
