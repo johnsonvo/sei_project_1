31e8b7ef70529af1e441fb4664bfed0595839e92
johnsonvo <JOHNSONMVO@GMAIL.COM>
2019-04-15 22:51:48 -0700

    Removed conflicts in index.js

--- models/index.js
+++ models/index.js
@@ -6,20 +6,7 @@ mongoose.connect(DB_URL, {useNewUrlParser: true, useFindAndModify: false})
   .catch((err) => console.log(err));
 
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> kanjamad-sprint1
-  Project: require('./project'),
-  User : require('./user'),
-  Order : require('./order'),
-  Flower : require('./flower'),
-<<<<<<< HEAD
-=======
   User: require('./user'),
   Order: require('./order'),
   Flower: require('./flower')
->>>>>>> 04790a1cb76b42bbbd4946936b6cf66bf6dd4915
-=======
->>>>>>> kanjamad-sprint1
 };
