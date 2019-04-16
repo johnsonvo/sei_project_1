4da088437fe394c7f8f9485de6a65b11e482a2e2
johnsonvo <JOHNSONMVO@GMAIL.COM>
2019-04-16 11:16:21 -0700

    Added seed data to test routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Database
+// Database
 mongoose.Promise = global.Promise;
 const db = require('./models');
 
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Error handling
+// Error handling - removed bc it's causing an error
 // app.use((req, res, next) => {
 //   const error = new Error('Not found');
 //   error.status = 404;
@@ -45,6 +45,51 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req,res) => {
     res.sendFile('views/index.html', {root: __dirname});
 });
+
+/////////////////////
+//// SEED DATA /////
+//// TODO: Delete////
+////////////////////
+const users_list = [
+  {
+    fullName: "Leanne Graham",
+    email: "Sincere@april.biz",
+    dob: "2/11/1988",
+    products: "Rose"
+  },
+  {
+    fullName: "Ervin Howell",
+    email: "Shanna@melissa.tv",
+    dob: "2/09/1900",
+    products: "Sunflower"
+  },
+];
+
+const flowers_list = [
+  {
+    name: "Aconite",
+    img:"https://proflowers.wpengine.com/wp-content/plugins/pf-flowertypes/image/winter-aconite-720790.jpg",
+    price: "$12",
+    season: "Early Spring",
+    orders: 12,
+  },
+];
+
+const orders_list = [
+  {
+    userId: 1,
+    quantity: 3,
+    price: "$100",
+    productId:12,
+
+  },
+  {
+    userId: 2,
+    quantity: 4,
+    price: "$200",
+    productId:13,
+  },
+];
 
 /////////////////////
 /// USERS ROUTES ///
@@ -87,35 +132,35 @@ app.delete('/api/users/:userId', (req, res) => {
 // FLOWERS ROUTES //
 ///////////////////
 // Get flower
-app.get('/api/products', (req, res) => {
-    res.status(200).json({ msg: 'Handling GET requests to /products' });
+app.get('/api/flowers', (req, res) => {
+    res.status(200).json({ msg: 'Handling GET requests to /flowers' });
 });
 
 // Create Flower
-app.post('/api/products', (req, res) => {
+app.post('/api/flowers', (req, res) => {
   const flower = {
     name: req.body.name,
     price: req.body.price
   };
   res.status(201).json({
-    msg: 'Handling POST requests to /products',
+    msg: 'Handling POST requests to /flowers',
     createdFlower: flower
   });
 });
 
 // Get Flower by ID
-app.get('/api/products/:id', (req, res) => {
-  res.status(200).json({ msg: 'Handling GET requests to /products by ID' });
+app.get('/api/flowers/:id', (req, res) => {
+  res.status(200).json({ msg: 'Handling GET requests to /flowers by ID' });
 });
 
 // Update Flower by ID
-app.put('/api/products/:id', (req, res) => {
-  res.status(200).json({ msg: 'Handling GET requests to /products by ID' });
+app.put('/api/flowers/:id', (req, res) => {
+  res.status(200).json({ msg: 'Handling GET requests to /flowers by ID' });
 });
 
 // Delete Flower by ID
-app.delete('/api/products/:id', (req, res) => {
-    res.status(200).json({ msg: 'Handling DELETE requests to /products by ID'});
+app.delete('/api/flowers/:id', (req, res) => {
+    res.status(200).json({ msg: 'Handling DELETE requests to /flowers by ID'});
 });
 
 
