5edbd09235f773e96ce76854481a6a1d88e89062
johnsonvo <JOHNSONMVO@GMAIL.COM>
2019-04-15 21:14:49 -0700

    Added CORS and error handling template

--- server.js
+++ server.js
@@ -12,6 +12,32 @@ const db = require('./models');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // this takes the json data recieved and accesses it through 'body'.
 
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// Error handling
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 //Serve Static Assets
 app.use(express.static(__dirname + '/public'));
 
