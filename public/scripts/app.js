a6f5ebd9a43ac3aa2b30a6b8bf0a6f77564f8aba
Jhrohman <josephhrohman@gmail.com>
2019-04-16 09:40:14 -0700

    Better defined MVP pages, created user flow (going from any page to any page without errors)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,39 @@
 console.log('sanity check');
 
+// Reveal Market
+$('#landingPage').click( () => {
+    $('.marketPlace').css('display', 'block');
+    $('#landingPage').css('display', 'none');
+    $('main').css('display', 'block');
+});
+
+$('#navMarket').click( () => {
+    $('.marketPlace').css('display', 'block');
+    $('.userCreatePage').css('display', 'none');
+    $('.flowerCreatePage').css('display', 'none');
+    $('.profilePage').css('display', 'none');
+});
+
+// Reveal User Create
+$('#navCreateUser').click( () => {
+    $('.userCreatePage').css('display', 'block');
+    $('.marketPlace').css('display', 'none');
+    $('.flowerCreatePage').css('display', 'none');
+    $('.profilePage').css('display', 'none');
+});
+
+// Reveal Profile
+$('#navProfile').click( () => {
+    $('.profilePage').css('display', 'block');
+    $('.userCreatePage').css('display', 'none');
+    $('.marketPlace').css('display', 'none');
+    $('.flowerCreatePage').css('display', 'none');
+});
+
+// Reveal Flower Create
+$('#navCreateFlower').click( () => {
+    $('.flowerCreatePage').css('display', 'block');
+    $('.marketPlace').css('display', 'none');
+    $('.userCreatePage').css('display', 'none');
+    $('.profilePage').css('display', 'none');
+});
