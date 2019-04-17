b6cf1203b6d014aa83722d138a55a2eddfccb3ce
kanjamad bosel <kanjamadpor@gmail.com>
2019-04-17 11:18:56 -0700

    fix ajax funtion call to be ES5

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ app.use((req, res, next) => {
 //   next(error);
 // });
 
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   });
+// });
 
 
 //Serve Static Assets
